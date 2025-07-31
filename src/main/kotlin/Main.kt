9b0adab5ae516beadf46b3f0b41fd418be2820b2
barafagnus <barafagnus@gmail.com>
2025-07-31 12:45:24 +0700

    Revert "Impl a parser of quotes from "livelib""
    This reverts commit cd00d383a2ead07bddc6b1bf90d869017128acb6.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,6 @@
 package org.example
 
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
-
 fun main() {
-    val doc: Document = Jsoup.connect("https://www.livelib.ru/book/1007081008/quotes-avtostopom-po-galaktike-duglas-adams/").get()
-    val elements: Elements = doc.select(".lenta-card p")
-
-    for (element: Element in elements) {
-        println(element.text())
-    }
+    println("Hello World!")
+    println("asd")
 }
